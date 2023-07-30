0caadb7f3585cfbf71d1a6a4554dff352fe787fa
seasons986 <annalin12345@gmail.com>
2023-07-30 13:57:53 -0400

    init

---
+++ index.js
@@ -0,0 +1,46 @@
+window.addEventListener("load", () => {
+  function getRandomInt(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min) + min); // The maximum is exclusive and the minimum is inclusive
+  }
+
+  let debounce = function(func, delay) {
+    clearTimeout(timeout);
+    timeout = setTimeout(func, delay);
+  };
+
+  let timeout;
+
+  // Fisher-Yates/Knuth shuffle code from https://stackoverflow.com/a/6274398
+  function translate(text) {
+    let words = text.split(" ");
+    if (words.length < 3) return;
+    let div = document.createElement("div");
+    let counter = words.length;
+    
+    for (let i = 0; i < 9; i++) {
+      while (counter > 0) {
+        let index = Math.floor(Math.random() * counter);
+        counter--;
+        let temp = words[counter];
+        words[counter] = words[index];
+        words[index] = temp;
+        div.innerHTML += (" " + words[counter]);
+      }
+      content.appendChild(div);
+      div = document.createElement("div");
+      counter = words.length;
+    }
+  }
+  
+  function onType(e) {
+    if (!e || !e.data || e.target.value.length < 3) return;
+    content.innerHTML = input.value;
+    debounce(() => translate(input.value), 300);
+  }
+
+  let content = document.getElementById("content");
+  let input = document.getElementById("prompt");
+  input.oninput = onType;
+});
